9c0c5794c59115c790817e7f2742b617132e2d3b
ritesh b <rbhondwe@gmail.com>
2025-11-03 16:13:10 +0530

    Update install_http.sh
    added sudo chown -R www-data:www-data /var/www/html

--- install_http.sh
+++ install_http.sh
@@ -4,4 +4,5 @@ sudo apt-get update -y
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
+sudo chown -R www-data:www-data /var/www/html
 echo "Hello from EC2 user data!" > /var/www/html/index.html
