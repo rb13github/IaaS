f045613ab9208cad59745eeccf38095d7374b826
ritesh b <rbhondwe@gmail.com>
2025-11-03 16:02:48 +0530

    Update install_http.sh
    httpd service start and enable

--- install_http.sh
+++ install_http.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+#sudo apt-get install -y httpd #for
+sudo apt-get install -y apache2
+sudo systemctl start httpd.service
+sudo systemctl enable httpd.service
 echo "Hello from EC2 user data!" > /var/www/html/index.html
