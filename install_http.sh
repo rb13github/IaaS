ad121e79b7ea07820bc53834aee2a546dc5b8760
ritesh b <rbhondwe@gmail.com>
2025-11-03 15:52:46 +0530

    Update install_http.sh
    apt-get  instead of yum for ubuntu

--- install_http.sh
+++ install_http.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-sudo yum update -y
-sudo yum install -y httpd
+sudo apt-get update -y
+sudo apt-get install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 echo "Hello from EC2 user data!" > /var/www/html/index.html
