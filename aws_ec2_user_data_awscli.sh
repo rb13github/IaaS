8d6a4c84b3570d550d39f17dfa41b863e2fc4c35
ritesh b <rbhondwe@gmail.com>
2025-11-04 12:02:29 +0530

    Create aws_ec2_user_data_awscli.sh
    user data script to be used for ec2 instance creation to install aws clie

---
+++ aws_ec2_user_data_awscli.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+cd /root
+
+# Update and install dependencies
+apt-get update -y
+apt-get install -y curl unzip jq
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+./aws/install
+ls
+aws --version
