dcc99c6615a18b9cd091a3dbaea787e63f47fea1
ritesh b <rbhondwe@gmail.com>
2025-11-06 12:27:21 +0530

    Update install_http.sh
    used sudo bash to create index.html

--- install_http.sh
+++ install_http.sh
@@ -7,3 +7,4 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo chown -R www-data:www-data /var/www/html
 echo "Hello from EC2 user data!" > /var/www/html/index.html
+sudo bash -c 'cat > /var/www/html/index.html <<EOF "Hello from EC2 instance index.html for apache!" EOF
